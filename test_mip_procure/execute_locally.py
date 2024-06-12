9fe80973efa14ff0640d9817368fb5ad1017af2c
Gabriel Michels <gabrielsehnem.15@gmail.com>
2024-06-12 10:49:49 -0300

    replace execute_locally.py by test_local_execution.py.

--- test_mip_procure/execute_locally.py
+++
@@ -1,4 +0,0 @@
-from mip_procure import input_schema
-path = 'data/inputs'
-dat = input_schema.csv.create_pan_dat(path)
-print(dat)
---
+++ test_mip_procure/test_local_execution.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from pprint import pprint
+from mip_procure.action_update_packing_cost import update_packing_cost_solve2
+from mip_procure import input_schema, output_schema
+from mip_procure import solve
+
+
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 4000)
+
+# Read input data
+input_path = 'data/inputs'
+dat = input_schema.csv.create_pan_dat(input_path)
+
+# Check input data
+data_type_failures = input_schema.find_data_type_failures(dat)
+print('data_type_failures:\n', data_type_failures)
+foreign_failures = input_schema.find_foreign_key_failures(dat)
+print('foreign_failures:\n', foreign_failures)
+duplicates = input_schema.find_duplicates(dat)
+print('duplicates:\n', duplicates)
+row_failures = input_schema.find_data_row_failures(dat)
+print('row_failures:', row_failures)
+
+# optimize
+sln = solve(dat)
+
+# Check output data
+print('\nCheck output data:\n')
+data_type_failures = output_schema.find_data_type_failures(sln)
+print('data_type_failures:\n', data_type_failures)
+foreign_failures = output_schema.find_foreign_key_failures(sln)
+print('foreign_failures:\n', foreign_failures)
+duplicates = output_schema.find_duplicates(sln)
+print('duplicates:\n', duplicates)
+row_failures = output_schema.find_data_row_failures(sln)
+print('row_failures:', row_failures)
+
+# Write output data
+output_path = 'data/outputs/'
+output_schema.csv.write_directory(sln, output_path)
+output_schema.xls.write_file(sln, output_path+'Output.xlsx')
