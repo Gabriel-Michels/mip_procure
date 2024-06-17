d9e8f044ec8eaa436462f749d31036c6ab5308c1
Gabriel Michels <gabrielsehnem.15@gmail.com>
2024-06-17 16:46:58 -0300

    Configure deploy for mipHub

--- mip_procure/__init__.py
+++ mip_procure/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.1.0"
 from mip_procure.main import solve
 from mip_procure.schemas import input_schema, output_schema
 from mip_procure.action_update_packing_cost import update_packing_cost_solve
-# from mip_me.action_report_builder import report_builder_solve (I don't know anything about this)
+
 
 actions_config = {
     'Update Packing Cost': {
@@ -11,16 +11,16 @@ actions_config = {
         'tooltip': "Update the packing cost by the factor entered in the 'Packing Cost Multiplier' parameter"},
     }
 
+
 parameters_config = {
     'hidden': list(),
-    'categories': dict(),
-    'order': ['AcquisitionLimitPeriod', 'MaxTimePackingPack', 'TransportingLimitByPeriod',
+    'categories': {'InventoryCapacities': ['InventoryCapacityPack', 'InventoryCapacityGourmet']},
+    'order': ['MaxTimePackingPack', 'TransportingLimitByPeriod',
               'InventoryCapacityGourmet', 'InventoryCapacityPack', 'Packing Cost Multiplier'],
     'tooltips': {
-        'AcquisitionLimitPeriod': 'The limit of Acquisition of packing in a period',
         'MaxTimePackingPack': 'The limit time of a packing stored in Patas Pack',
         'TransportingLimitByPeriod': 'The maximum of packing that can be transported from Patas Pack to Pet Gourmet',
-        'InventoryCapacityGourmet': 'The maximum quantity of packing in Pet Gourmet',
+        'InventoryCapacityGourmet': 'The maximum quantity of  packing in Pet Gourmet',
         'InventoryCapacityPack': 'The maximum quantity of packing in Patas Pack',
         'Packing Cost Multiplier': 'The factor that can be used by the Update Packing Cost'
         }
@@ -31,9 +31,10 @@ input_tables_config = {
     'hidden_tables': ['parameters'],
     'categories': dict(),
     'order': list(),
-    'tables_display_names': dict(),
-    'columns_display_names': dict(),
-    'hidden_columns': {'packing': ['Color']
+    'tables_display_names': {'demand_packing': 'Demand Packing', 'items_aging': 'Items Aging'},
+    'columns_display_names': {'distribution': {'Minimum Transfer Qty': 'Minimum Transfer Quantity',
+                                               'Maximum Transfer Qty': 'Maximum Transfer Quantity'} },
+    'hidden_columns': {'packing': ['Color', 'Size']
                        }
     }
 
