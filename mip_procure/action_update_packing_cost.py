f4dda45ae6748f3b1b9a5dc37aa7ee5e2f044a6b
Gabriel Michels <gabrielsehnem.15@gmail.com>
2024-07-11 17:00:58 -0300

    Remove function update_packing_cost_solve2.

--- mip_procure/action_update_packing_cost.py
+++ mip_procure/action_update_packing_cost.py
@@ -1,15 +1,5 @@
 from mip_procure import input_schema
 
-
-def update_packing_cost_solve2(dat):
-    """Increases packing cost by 20%"""
-    packing = dat.packing.copy()
-    packing['Unit Price'] = 1.2 * packing['Unit Price']
-    packing = packing.round({'Unit Price': 2})
-    dat.packing = packing[['Packing ID', 'Unit Price', 'Size', 'Color']]
-    return dat
-
-
 def update_packing_cost_solve(dat):
     """Multiply packing cost by the Packing Cost Multiplier parameter"""
     params = input_schema.create_full_parameters_dict(dat)
